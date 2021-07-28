86b25ad0aae77402df0c767028f5aaee25da26a3
chrisbite121@yahoo.co.uk <chrisbite121@yahoo.co.uk>
2021-07-28 11:43:41 +0100

    first commit on this control

---
+++ wellingtonefieldformatter/components/FieldComponent.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import {useState, useEffect} from 'react';
+import { ITextFieldProps, TextField } from '@fluentui/react';
+
+export const FieldComponent: React.FC = (props:any) => {
+  const [value, setValue] = useState('')
+
+  useEffect(() => {
+    if(typeof props.value == 'number' 
+    && props.value !== value)
+      setValue(props.value);
+
+    if(props.value == undefined)
+      setValue('')
+  },[props.value])
+
+
+  const checkValue = (value:any) => {
+    if(value == undefined || !isNaN(+value))
+    {
+      setValue(value);
+      props.onChange(+value)
+    }
+  }
+
+  return (
+    <TextField
+        value={value}
+        style={{backgroundColor: props.bgColour, color: props.fColour}}
+        onChange={(e:any) => checkValue(e.target.value)}
+      />
+    );
+  }
